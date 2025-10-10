9aa329e820d82557ba1d79e39579273d5865641c
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-10 16:37:37 +0200

    ejercicio4

--- ejercicio3.js
+++ ejercicio3.js
@@ -2,9 +2,13 @@ const mostrarSuma = () => {
     this.event.preventDefault();
     const numero = Number(document.getElementById('numero').value);
     let suma = 0;
-    for (let i = 1; i <= numero; i++) {
+    if (isNaN(numero)){
+        alert("Por favor, introduce un número válido.");
+        return;
+    } else {for (let i = 1; i <= numero; i++) {
         suma += i;
     }
+}
     let resultado = document.getElementById("resultado")
     resultado.innerHTML=`La suma de los números del 1 al ${numero} es ${suma}`;
 }
