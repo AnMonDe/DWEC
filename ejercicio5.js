bbe61004d953ba8952433c9d65a093f55323c6cb
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-11 13:24:53 +0200

    ejercicio5 terminado funcionando todo

--- ejercicio5.js
+++ ejercicio5.js
@@ -5,10 +5,12 @@ const mostrarTablas = () =>{
     let tablaBase = `<h2>Tablas de multiplicar hasta el ${numero}.</h2>`;
     tablaBase += '<ul>'
     for (i = 0; i<=numero;i++){
-        for (j = 0; j<=numero;j++){
-            tablaBase += `<li><h2>${i} * ${j} = ${i*j}</h2></li>`;
+        tablaBase += `<li><h2>Tabla del ${i}.</h2></li>`
+        for (j = 0; j<=10;j++){
+            tablaBase += `<li><h3>${i} * ${j} = ${i*j}</h3></li>`;
         }
     }
     resultado.innerHTML = tablaBase
     tablaBase += '</ul>'
+    document.getElementById('numero').value = '';
 }
