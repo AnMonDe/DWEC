9940d8453a508bfd6a91db2dab4eb854107b4a24
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-28 23:56:29 +0100

    ejercicio2 html

---
+++ Tarea3/ejercicio2.js
@@ -0,0 +1,7 @@
+let entrada_codificar = document.getElementById('codificar').value;
+let entrada_descodificar = document.getElementById('descodificar');
+let boton = document.getElementById('boton')
+
+boton.addEventListener('click',()=>{
+    console.log(entrada_codificar);
+})
