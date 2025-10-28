b558fc4e1474c54435b7387b7e4769e91ff74b00
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-29 00:20:06 +0100

    ejercicio2 html terminado y base js

--- Tarea3/ejercicio2.js
+++ Tarea3/ejercicio2.js
@@ -1,7 +1,14 @@
-let entrada_codificar = document.getElementById('codificar').value;
-let entrada_descodificar = document.getElementById('descodificar');
-let boton = document.getElementById('boton')
+let boton_cod = document.getElementById('boton_cod');
+let boton_desc = document.getElementById('boton_desc');
 
-boton.addEventListener('click',()=>{
-    console.log(entrada_codificar);
+boton_cod.addEventListener('click',()=>{
+    let entrada_codificar = document.getElementById('codificar').value;
+    let salida_cod = document.getElementById('salida_cod');
+    salida_cod.innerHTML = entrada_codificar;
+})
+
+boton_desc.addEventListener('click',()=>{
+    let entrada_descodificar = document.getElementById('descodificar').value;
+    let salida_desc = document.getElementById('salida_desc');
+    salida_desc.innerHTML = entrada_descodificar;
 })
