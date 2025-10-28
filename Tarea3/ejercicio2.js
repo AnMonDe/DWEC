50790702eb8fc0df2901ac17696d1d0b4c8a097a
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-29 00:24:02 +0100

    ejercicio2 html sin problemas

--- Tarea3/ejercicio2.js
+++ Tarea3/ejercicio2.js
@@ -4,11 +4,13 @@ let boton_desc = document.getElementById('boton_desc');
 boton_cod.addEventListener('click',()=>{
     let entrada_codificar = document.getElementById('codificar').value;
     let salida_cod = document.getElementById('salida_cod');
+    
     salida_cod.innerHTML = entrada_codificar;
 })
 
 boton_desc.addEventListener('click',()=>{
     let entrada_descodificar = document.getElementById('descodificar').value;
     let salida_desc = document.getElementById('salida_desc');
+
     salida_desc.innerHTML = entrada_descodificar;
 })
