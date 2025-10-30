146fbafe5d5d8fceb4f22ad6c131b6a4ec819a1f
AnMonDe <3855415@alu.murciaeduca.es>
2025-10-30 22:13:39 +0100

    ejercicio3 terminado

--- Tarea3/ejercicio3.js
+++ Tarea3/ejercicio3.js
@@ -1,6 +1,10 @@
 document.getElementById("salida").innerHTML = localStorage.contador;
 
 function contadorClick(){
+    if (Number(localStorage.contador)>=Number.MAX_SAFE_INTEGER){
+        alert("Ya no se pueden realizar nmás clicks");
+        return;
+    }
     if(localStorage.contador){
         localStorage.contador = Number(localStorage.contador)+1;
     } else {
