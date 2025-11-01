0c73bd47d656b5ef07a39a8f83549988228c51e8
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-01 11:09:49 +0100

    ejercicio4 comentado

--- Tarea3/ejercicio4.js
+++ Tarea3/ejercicio4.js
@@ -1,3 +1,3 @@
-setTimeout(function () {
-        window.location = "https://www.google.com";
-    }, 3000);
+setTimeout(function () { //Funcion de tiempo
+        window.location = "https://www.google.com"; //Redireccion de pagina
+    }, 3000); //Tiempo de espera
