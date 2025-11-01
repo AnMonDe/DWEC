1f63851039c2b075f307379a15a723eceb85d6b4
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-01 10:46:28 +0100

    ejercicio3 comentado

--- Tarea3/ejercicio3.js
+++ Tarea3/ejercicio3.js
@@ -1,15 +1,15 @@
-document.getElementById("salida").innerHTML = localStorage.contador;
+document.getElementById("salida").innerHTML = localStorage.contador; //Muestra en pantalla la cantidad almacenada
 
-function contadorClick(){
-    if (Number(localStorage.contador)>=Number.MAX_SAFE_INTEGER){
-        alert("Ya no se pueden realizar nmás clicks");
-        return;
+function contadorClick(){ //Funcion contadora
+    if (Number(localStorage.contador)>=Number.MAX_SAFE_INTEGER){ //Inicio condicional numero preciso
+        alert("Ya no se pueden realizar nmás clicks"); //Alerta si pasa del numero preciso
+        return; //Cierre de la funcion
     }
-    if(localStorage.contador){
-        localStorage.contador = Number(localStorage.contador)+1;
-    } else {
-        localStorage.contador = 1;
+    if(localStorage.contador){ //Inicio condicional contador
+        localStorage.contador = Number(localStorage.contador)+1; //Suma uno al contador
+    } else { //No se cumple el condicional
+        localStorage.contador = 1; //Inicia el contador 
     }
-    document.getElementById("salida").innerHTML = localStorage.contador;
+    document.getElementById("salida").innerHTML = localStorage.contador; //Muestra en pantalla la actualizacion
 }
 
