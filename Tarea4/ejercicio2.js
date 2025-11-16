5005d55b60c601bcb7f6fefe9cd9b8b3c458130c
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-16 01:30:23 +0100

    ejercicio9 base

--- Tarea4/ejercicio2.js
+++ Tarea4/ejercicio2.js
@@ -11,13 +11,12 @@ class Banco{
     }
 
     crearCuenta(codigo, saldo=0){
+        let clave = codigo.toString().padStart(6,"0");
         
         if(codigo<1||codigo>599999){
             console.error("Codigo fuera de rango");
             return;
         }
-
-        let clave = codigo.toString().padStart(6,"0");
 
         this.cuentas[clave] = saldo;
         console.log(`${clave} - ${saldo}€`);
@@ -25,20 +24,28 @@ class Banco{
     }
 
     actualizarCuenta(codigo, ingreso){
-
         let clave = codigo.toString().padStart(6, "0");
 
         if(!this.cuentas.hasOwnProperty(clave)){
-            console.error("La cuenta no existe")
+            console.log("La cuenta no existe");
             return;
         }
 
         this.cuentas[clave] += ingreso;
         console.log(`Cuenta ${clave} actualizada. Saldo ${this.cuentas[clave]}`);
-        
+
     }
 
-    eliminarCuenta(){
+    eliminarCuenta(codigo){
+        let clave = codigo.toString().padStart(6, "0");
+
+        if(!this.cuentas.hasOwnProperty(clave)){
+            console.log(`La cuenta no existe`);
+            return;
+        }
+
+        delete this.cuentas[clave];
+        console.log(`Cuenta ${clave} eliminada`);
 
     }
 
@@ -58,8 +65,17 @@ class Banco{
 }
 
 let miBanco = new Banco("Andy");
+
 miBanco.crearCuenta(111111,55);
 miBanco.crearCuenta(222222);
 miBanco.crearCuenta(333333,66);
+miBanco.crearCuenta(444444, 20);
+
 miBanco.actualizarCuenta(111111,22);
+miBanco.actualizarCuenta(222222, 100);
+miBanco.actualizarCuenta(555555, -50);
+
+miBanco.eliminarCuenta(444444);
+miBanco.eliminarCuenta(555555);
+
 miBanco.listarCuenta();
