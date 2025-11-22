6d8a0d401259cd74061fc3a18008a3241ad34a0a
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-23 00:00:07 +0100

    ejercicio1.4

--- Tarea5/ejercicio1.js
+++ Tarea5/ejercicio1.js
@@ -14,7 +14,13 @@ let ultimoArticulo = articulos[articulos.length -1];
 ultimoArticulo.style.display = "none";
 
 //4. Recorre los artículos y añade al inicio de la cabecera el número de noticia (1- Hospitalizado...)
+articulos.forEach((articulo, index) => {
+    let primerDiv = articulo.querySelector("div");
+    if(primerDiv){
+        primerDiv.prepend((index + 1) + "- ")
+    }
+});
 
 //5. Añade la clase cabecera (ya esta declarada en los estilos), al primer div de cada artículo.
 
-//6. Busca en todos los artículos donde aparece la cadena "Servicio Murciano de Salud", y sustitúyela por "S.M.S" (¿método replace de cadenas?).l
+//6. Busca en todos los artículos donde aparece la cadena "Servicio Murciano de Salud", y sustitúyela por "S.M.S" (¿método replace de cadenas?).
