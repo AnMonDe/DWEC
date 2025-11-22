012e311ad9e10bc56e0afcfdc40c1d184f5f2399
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-22 19:17:32 +0100

    ejercicio1.1

---
+++ Tarea5/ejercicio1.js
@@ -0,0 +1,14 @@
+//1. Modifica el contenido del título, poniéndole tu nombre.
+let titulo = document.getElementById("titulo")
+let nombre = "Andrés Monje Delgado";
+titulo.innerText=nombre;
+
+//2. Modifica la imagen que muestra el segundo artículo artículo
+
+//3. Oculta el último articulo.
+
+//4. Recorre los artículos y añade al inicio de la cabecera el número de noticia (1- Hospitalizado...)
+
+//5. Añade la clase cabecera (ya esta declarada en los estilos), al primer div de cada artículo.
+
+//6. Busca en todos los artículos donde aparece la cadena "Servicio Murciano de Salud", y sustitúyela por "S.M.S" (¿método replace de cadenas?).
