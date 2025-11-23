922d0382d47cd15c36bf5a84b147fb564816009d
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-23 12:22:12 +0100

    ejercicio2.2

--- Tarea5/ejercicio2.js
+++ Tarea5/ejercicio2.js
@@ -4,11 +4,20 @@ let titulo = document.createElement("h1");
 titulo.innerText = nombre;
 document.body.appendChild(titulo);
 
-
 //2. Un elemento contenedor, que contendrá los artículos de la tienda.
+let contenedor = document.createElement("div");
+contenedor.className = "contenedor";
+document.body.appendChild(contenedor);
 
 //3. Cada artículo contiene un nombre, una descripción, un precio y una imagen. Estos datos los leemos 
+//del array datos, que tenemos disponible en el archivo datos.js y ya se encuentra cargado en la web.
+for(let i=0;i<datos.length;i++){
+    let contenedorInterno = document.createElement("div");
+    contenedorInterno.className = "resultado";
+    contenedorInterno.innerHTML = `<h3>${datos[i].nombre}</h3><p>${datos[i].descripcion}</p>
+    <p>${datos[i].precio}</p><img src="${datos[i].imagen}">`;
+    contenedor.appendChild(contenedorInterno);
+}
 
-//del array datos, que tenemos disponible en el archivo datos.js y ya se encuentra cargado en la web
-
-//4. Añade las clases necesarias a los elementos y completa el archivo estilos.css para conseguir un aspecto similar al del enunciado.
+//4. Añade las clases necesarias a los elementos y completa el archivo estilos.css para conseguir 
+// un aspecto similar al del enunciado.
