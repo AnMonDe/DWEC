9a7dbd12b1c725c928422857cfd55fda9a2ce467
AnMonDe <3855415@alu.murciaeduca.es>
2025-11-23 11:47:50 +0100

    ejercicio2.1

---
+++ Tarea5/ejercicio2.js
@@ -0,0 +1,14 @@
+//1. Un título con tu nombre.
+let nombre = "Andrés Monje Delgado";
+let titulo = document.createElement("h1");
+titulo.innerText = nombre;
+document.body.appendChild(titulo);
+
+
+//2. Un elemento contenedor, que contendrá los artículos de la tienda.
+
+//3. Cada artículo contiene un nombre, una descripción, un precio y una imagen. Estos datos los leemos 
+
+//del array datos, que tenemos disponible en el archivo datos.js y ya se encuentra cargado en la web
+
+//4. Añade las clases necesarias a los elementos y completa el archivo estilos.css para conseguir un aspecto similar al del enunciado.
