11183bac50dca0bb44af3c5e4fec3829713f4e99
AndyMonCode <z3st3r_2@hotmail.com>
2026-01-25 11:57:01 +0100

    ejercicio1 datos en la tabla

---
+++ Tarea7/ejercicio1.js
@@ -0,0 +1,33 @@
+let url = "https://jsonplaceholder.typicode.com/users"
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(datos => mostrarDatos(datos))
+    //.then(datos => console.log(datos))
+
+let boton = document.getElementById("buscar")
+
+boton.addEventListener("click",()=>{
+    let entrada = document.getElementById("entrada").value
+
+})
+
+function mostrarDatos(datos){
+    let cuerpo = document.getElementById("tabla")
+    for(let i in datos){
+        let fila = document.createElement("tr")
+
+        let celdaNombre = document.createElement("td")
+        celdaNombre.innerHTML = datos[i].name
+        fila.appendChild(celdaNombre)
+
+        let celdaCalle = document.createElement("td")
+        celdaCalle.innerHTML = datos[i].address.street
+        fila.appendChild(celdaCalle)
+
+        let celdaCiudad = document.createElement("td")
+        celdaCiudad.innerHTML = datos[i].address.city
+        fila.appendChild(celdaCiudad)
+
+        cuerpo.appendChild(fila)
+    }
+}
