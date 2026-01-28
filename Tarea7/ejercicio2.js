b5374c3267707a6af195307ae82c0aceb8bfc560
AndyMonCode <z3st3r_2@hotmail.com>
2026-01-28 23:08:53 +0100

    ejercicio 2 tipo

--- Tarea7/ejercicio2.js
+++ Tarea7/ejercicio2.js
@@ -3,20 +3,35 @@ let imagenes = "http://api.raulserranoweb.es/imagenes_art/"
 let pagina = document.getElementById("salida")
 let tipo = document.getElementById("tipo")
 
-fetch(informacion)
+function cargar(categoria){
+    let url =informacion
+
+    if(categoria !== "Todas"){
+        url += `?cat=${categoria}`
+    }
+
+    fetch(url)
     .then(respuesta => respuesta.json())
-    .then(datos => {console.log(datos)
-    
-    for(let i = 0; i < datos.length; i++){
-        let seccion = document.createElement("div")
-        let imagen = imagenes + datos[i].cod
-        seccion.className = "seccion"
-        seccion.innerHTML = `<img src="${imagen}" <br>
-        Nombre: ${datos[i].nom} <br>
-        Descripcion: ${datos[i].des} <br>
-        Categoria: ${datos[i].cat}`
-        pagina.appendChild(seccion)}
+        .then(datos => {console.log(datos)
+
+            pagina.innerHTML = ""
+        
+        for(let i = 0; i < datos.length; i++){
+            let seccion = document.createElement("div")
+            let imagen = imagenes + datos[i].cod
+            seccion.className = "seccion"
+            seccion.innerHTML = `<img src="${imagen}" <br>
+            Nombre: ${datos[i].nom} <br>
+            Descripcion: ${datos[i].des} <br>
+            Categoria: ${datos[i].cat}`
+            pagina.appendChild(seccion)
+        }
     })
+}
 
+cargar()
 
+tipo.addEventListener("change", function(){
+    cargar(this.value)
+})
 
