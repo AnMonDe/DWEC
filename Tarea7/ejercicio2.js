6d886f833739df9d40f419581795af2aeb5ac298
AndyMonCode <z3st3r_2@hotmail.com>
2026-01-28 22:55:00 +0100

    ejercicio2 imagenes

--- Tarea7/ejercicio2.js
+++ Tarea7/ejercicio2.js
@@ -1,5 +1,5 @@
 let informacion = "http://api.raulserranoweb.es/rest.php"
-let imagenes = "http://api.raulserranoweb.es/imagenes_art/XXX"
+let imagenes = "http://api.raulserranoweb.es/imagenes_art/"
 let pagina = document.getElementById("salida")
 let tipo = document.getElementById("tipo")
 
@@ -9,7 +9,7 @@ fetch(informacion)
     
     for(let i = 0; i < datos.length; i++){
         let seccion = document.createElement("div")
-        let imagen = imagenes.replace("XXX",datos[i].cod)
+        let imagen = imagenes + datos[i].cod
         seccion.className = "seccion"
         seccion.innerHTML = `<img src="${imagen}" <br>
         Nombre: ${datos[i].nom} <br>
