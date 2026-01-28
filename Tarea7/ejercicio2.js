c1ed4323454c5b49c30b909db690d6ff10d7b31b
AndyMonCode <z3st3r_2@hotmail.com>
2026-01-28 22:52:26 +0100

    ejercicio2 imagenes

--- Tarea7/ejercicio2.js
+++ Tarea7/ejercicio2.js
@@ -1,14 +1,18 @@
-let url = "http://api.raulserranoweb.es/rest.php"
+let informacion = "http://api.raulserranoweb.es/rest.php"
+let imagenes = "http://api.raulserranoweb.es/imagenes_art/XXX"
 let pagina = document.getElementById("salida")
+let tipo = document.getElementById("tipo")
 
-fetch(url)
+fetch(informacion)
     .then(respuesta => respuesta.json())
     .then(datos => {console.log(datos)
     
     for(let i = 0; i < datos.length; i++){
         let seccion = document.createElement("div")
+        let imagen = imagenes.replace("XXX",datos[i].cod)
         seccion.className = "seccion"
-        seccion.innerHTML = `Nombre: ${datos[i].nom} <br>
+        seccion.innerHTML = `<img src="${imagen}" <br>
+        Nombre: ${datos[i].nom} <br>
         Descripcion: ${datos[i].des} <br>
         Categoria: ${datos[i].cat}`
         pagina.appendChild(seccion)}
